b8ac9887c347a9f52aab26ad249e77d47b48a978
Mary-Njiru <marynwanjiruu@gmail.com>
2024-02-06 22:55:22 +0300

    added one kotlin file

---
+++ Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var fullName = "Mary Njiru"
+    println(fullName)
+
+    var age = 21
+    println(age)
+
+    var phoneNumber = 790250247
+    println(phoneNumber)
+
+    var weight = 53.5
+    println(weight)
+
+    var citizen = "Kenyan"
+    println(citizen)
+
+
+}
